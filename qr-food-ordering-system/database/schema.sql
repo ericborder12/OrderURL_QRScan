8d88384f525b1c99f040a01441526ecf04ff6d8d
ericborder12 <153516972+ericborder12@users.noreply.github.com>
2026-07-13 12:57:40 +0700

    Update schema.sql

--- qr-food-ordering-system/database/schema.sql
+++ qr-food-ordering-system/database/schema.sql
@@ -1,1 +1,12 @@
+CREATE TABLE restaurants (
+    restaurant_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    logo VARCHAR(255)
+);
 
+CREATE TABLE menu_items (
+    item_id INT PRIMARY KEY,
+    restaurant_id INT,
+    name VARCHAR(100),
+    price DECIMAL(10,2)
+);
