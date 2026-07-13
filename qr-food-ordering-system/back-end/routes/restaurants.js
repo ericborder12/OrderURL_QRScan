354003e20230ffe076cebe635dda07fc24bf84db
ericborder12 <153516972+ericborder12@users.noreply.github.com>
2026-07-13 13:15:32 +0700

    Add routes for restaurants

--- qr-food-ordering-system/back-end/routes/restaurants.js
+++ qr-food-ordering-system/back-end/routes/restaurants.js
@@ -1,1 +1,29 @@
+const express = require("express");
 
+const router = express.Router();
+
+// Get all restaurants
+router.get("/", (req,res)=>{
+    res.json([
+        {
+            id:1,
+            name:"Sample Coffee Shop"
+        }
+    ]);
+
+});
+
+// Create restaurant
+router.post("/",(req,res)=>{
+
+    const restaurant = req.body;
+
+    res.json({
+        message:"Restaurant created",
+        data:restaurant
+    });
+
+});
+
+
+module.exports = router;
